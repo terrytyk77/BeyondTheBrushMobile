7608253387efe46374e467758110a054185a86d2
DESKTOP-O32329F\Darkness <terrytyk77@gmail.com>
2020-12-04 13:04:32 +0000

    Login Screen Added !

---
+++ app/src/main/java/com/beyondthebrushmobile/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.beyondthebrushmobile
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun login(view: View){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
