778a1082b207f1127548a55bbb7a3d2163a397cc
DESKTOP-O32329F\Darkness <terrytyk77@gmail.com>
2020-12-04 01:51:06 +0000

    Splash Screen Added!

---
+++ app/src/main/java/com/beyondthebrushmobile/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.beyondthebrushmobile
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 3000)
+
+    }
+}
