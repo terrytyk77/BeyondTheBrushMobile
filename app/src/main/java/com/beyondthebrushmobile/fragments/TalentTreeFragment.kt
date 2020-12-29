dde6d1e764f9041ce0e9ede8294a9735e29b0cb1
prooheckcp <vasco.soares.2001@gmail.com>
2020-12-29 20:35:35 +0000

    Completely fixed the tint for the buttons

--- app/src/main/java/com/beyondthebrushmobile/fragments/TalentTreeFragment.kt
+++ app/src/main/java/com/beyondthebrushmobile/fragments/TalentTreeFragment.kt
@@ -1,7 +1,6 @@
 package com.beyondthebrushmobile.fragments
 
 import android.graphics.Color
-import android.graphics.ColorFilter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -21,11 +20,77 @@ class TalentTreeFragment: Fragment(R.layout.fragment_talent_tree) {
                               savedInstanceState: Bundle?): View?
     {
 
-        view?.findViewById<ImageView>(R.id.node0)?.setColorFilter( Color.parseColor(NodeCalculation.colorUnavailable))
-
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_talent_tree, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        updateSlots()
+        println(view?.findViewById(R.id.node0))
+
+    }
+
+
+    //Update the talent tree slots
+    fun updateSlots(){
+        //Store the nodes
+        val ownedNodes = mutableListOf<ImageView?>()
+        val lockableNodes = mutableListOf<ImageView?>()
+        val unavailableNodes = mutableListOf<ImageView?>()
+
+        //Switch function to handle elements
+        fun nodeSwitch(elementID : Int){
+            val nodeView = view?.findViewById<ImageView>(elementID)
+            val nodeName = resources.getResourceEntryName(elementID)
+
+            //Get the correct nodes on the respective places
+            when(NodeCalculation.getNodeStatus(nodeName)){
+                "type0"->{
+                    ownedNodes.add(nodeView)
+                }
+                "type1"->{
+                    lockableNodes.add(nodeView)
+                }
+                "type2"->{
+                    unavailableNodes.add(nodeView)
+                }
+            }
+        }
+
+        //Set the nodes to their according lists
+        nodeSwitch(R.id.node0)
+        nodeSwitch(R.id.node1)
+        nodeSwitch(R.id.node2)
+        nodeSwitch(R.id.node3)
+        nodeSwitch(R.id.node4)
+        nodeSwitch(R.id.node5)
+        nodeSwitch(R.id.node6)
+        nodeSwitch(R.id.node7)
+        nodeSwitch(R.id.node8)
+        nodeSwitch(R.id.node9)
+        nodeSwitch(R.id.node10)
+
+
+        //Loop through the owned nodes
+        for(node in ownedNodes){
+            node?.setColorFilter( Color.parseColor(NodeCalculation.colorOwned) )
+            node?.isClickable = false
+        }
+
+        //Loop through the lockable nodes
+        for(node in lockableNodes){
+            node?.setColorFilter( Color.parseColor(NodeCalculation.colorLockable) )
+            node?.isClickable = true
+        }
+
+        //Loop through the unavailable nodes
+        for(node in unavailableNodes){
+            node?.setColorFilter( Color.parseColor(NodeCalculation.colorUnavailable) )
+            node?.isClickable = false
+        }
+
     }
 
 }
